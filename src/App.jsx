613d399e60a1c346729b617e04f56fa824af5483
devincello <eventosweb@hmamail.com>
2018-07-11 23:49:16 +0000

    preparado para integrar material design

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
+
+
+
+  const App = () => (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
@@ -15,7 +13,6 @@ class App extends Component {
         </p>
       </div>
     );
-  }
-}
+
 
 export default App;
